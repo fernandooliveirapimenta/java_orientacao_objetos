86a40658d81e42384294ea8975a4bacaa12e9d5a
Fernando Pimenta <fernando.pimenta107@gmail.com>
2019-06-21 15:08:49 -0300

    first commit

---
+++ src/main/java/br/com/fernando/java_orientacao_objetos/JavaOrientacaoObjetosApplication.java
@@ -0,0 +1,13 @@
+package br.com.fernando.java_orientacao_objetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOrientacaoObjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaOrientacaoObjetosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fernando/java_orientacao_objetos/JavaOrientacaoObjetosApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.fernando.java_orientacao_objetos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaOrientacaoObjetosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
